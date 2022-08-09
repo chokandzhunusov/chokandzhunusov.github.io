dc4119f8dc088eb7469684d5c260cea80becbf1e
Chokan <chokan.dzhunusov@gmail.com>
2022-08-09 19:47:11 +0300

    ADD: try catch

--- index.js
+++ index.js
@@ -2,26 +2,29 @@ const wait = ms => new Promise(resolve => setTimeout(resolve, ms));
 
 
 async function playSystemDesign() {
-    const rate = document.querySelector('#rate');
-    const synth = window.speechSynthesis;
-    if (synth.speaking) {
-        synth.cancel();
-    }
+    try {
+        const synth = window.speechSynthesis;
+        if (synth.speaking) {
+            synth.cancel();
+        }
 
-    const defaultTimeInterval = 40000;
-    await wait(2000);
-    for (let i = 0; i < cards.length; i++) {
-        const utterFront = new SpeechSynthesisUtterance(cards[i]['front']);
-        // utterFront.rate = rate.value;
-        utterFront.rate = 0.8;
-        synth.speak(utterFront);
-        await wait(cards[i]['playAfter'] || defaultTimeInterval);
+        const defaultTimeInterval = 40000;
+        await wait(2000);
+        for (let i = 0; i < cards.length; i++) {
+            const utterFront = new SpeechSynthesisUtterance(cards[i]['front']);
+            // utterFront.rate = rate.value;
+            utterFront.rate = 0.8;
+            synth.speak(utterFront);
+            await wait(cards[i]['playAfter'] || defaultTimeInterval);
 
-        const utterBack = new SpeechSynthesisUtterance(cards[i]['back']);
-        // utterBack.rate = rate.value;
-        utterBack.rate = 0.8;
-        synth.speak(utterBack);
-        await wait(cards[i]['playAfter'] || defaultTimeInterval);
+            const utterBack = new SpeechSynthesisUtterance(cards[i]['back']);
+            // utterBack.rate = rate.value;
+            utterBack.rate = 0.8;
+            synth.speak(utterBack);
+            await wait(cards[i]['playAfter'] || defaultTimeInterval);
+        }
+    } catch (e) {
+        alert(e)
     }
 }
 
