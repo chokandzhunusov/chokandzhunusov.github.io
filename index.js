0d5ed5e372e19abf23877a747356397169af40cb
Chokan <chokan.dzhunusov@gmail.com>
2022-08-10 20:33:46 +0300

    ADD: working ver

--- index.js
+++ index.js
@@ -105,61 +105,36 @@ const cards = [
 ]
 
 
-const wait = ms => new Promise(resolve => setTimeout(resolve, ms));
+function playSystemDesign() {
+    let t = 0
+    cards.forEach(card => {
+        setTimeout(() => {
+            console.log(card['front']);
+            window.speechSynthesis.speak(new SpeechSynthesisUtterance(card['front']));
+        }, t + card['playAfter'] || t)
 
-
-async function playSystemDesign() {
-    try {
-        const synth = window.speechSynthesis;
-        if (synth.speaking) {
-            synth.cancel();
-        }
-
-        const defaultTimeInterval = 40000;
-        await wait(2000);
-        for (let i = 0; i < cards.length; i++) {
-            const utterFront = new SpeechSynthesisUtterance(cards[i]['front']);
-            // utterFront.rate = rate.value;
-            utterFront.rate = 0.8;
-            synth.speak(utterFront);
-            await wait(cards[i]['playAfter'] || defaultTimeInterval);
-
-            const utterBack = new SpeechSynthesisUtterance(cards[i]['back']);
-            // utterBack.rate = rate.value;
-            utterBack.rate = 0.8;
-            synth.speak(utterBack);
-            await wait(cards[i]['playAfter'] || defaultTimeInterval);
-        }
-    } catch (e) {
-        alert(e)
-    }
+        console.log(t + card['playAfter'] || t);
+        t += 45000
+        setTimeout(() => {
+            console.log(card['back']);
+            window.speechSynthesis.speak(new SpeechSynthesisUtterance(card['back']));
+        }, t + card['playAfter'] || t)
+        console.log(t + card['playAfter'] || t);
+        t += 45000
+    })
 }
 
 
 async function playQuestions() {
-    const rate = document.querySelector('#rate');
-    const synth = window.speechSynthesis;
-    if (synth.speaking) {
-        synth.cancel();
-    }
-
-    const defaultTimeInterval = 90000;
-    await wait(2000);
-    for (let i = 0; i < questions.length; i++) {
-        const utterFront = new SpeechSynthesisUtterance(questions[i]['front']);
-        // utterFront.rate = rate.value;
-        utterFront.rate = 0.8;
-        synth.speak(utterFront);
-        await wait(questions[i]['playAfter'] || defaultTimeInterval);
-
-        const utterBack = new SpeechSynthesisUtterance(questions[i]['back']);
-        // utterBack.rate = rate.value;
-        utterBack.rate = 0.8;
-        synth.speak(utterBack);
-        await wait(questions[i]['playAfter'] || defaultTimeInterval);
-    }
-}
-
-async function test() {
-    alert('Works');
+    let t = 3000
+    cards.forEach(card => {
+        setTimeout(() => {
+            console.log(card['front']);
+        }, t)
+        t += 3000
+        setTimeout(() => {
+            console.log(card['back']);
+        }, t)
+        t += 3000
+    })
 }
