1caf99cc60c24bd259c8dd5ff9e50e7c505e0f7b
Chokan <chokan.dzhunusov@gmail.com>
2022-08-10 16:51:33 +0300

    Update

--- questions.js
+++
@@ -1,8 +0,0 @@
-const questions = [
-    {
-        'front': 'Question - Merge Overlapping Intervals',
-        'back': `Sort intervals by their starting values. 
-                Traverse Intervals and compare the start of next interval to end of current interval for overlap.
-                If overlap found mutate current interval, to merge the next interval into it.`,
-    },
-]
