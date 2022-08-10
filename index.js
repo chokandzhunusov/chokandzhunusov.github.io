a316df20d1f3a96dc1337bf8af23ef9469c02a9d
Chokan <chokan.dzhunusov@gmail.com>
2022-08-10 16:56:16 +0300

    ADD: await for next play

--- index.js
+++ index.js
@@ -108,7 +108,7 @@ const cards = [
 const wait = ms => new Promise(resolve => setTimeout(resolve, ms));
 
 
-function playSystemDesign() {
+async function playSystemDesign() {
     try {
         const synth = window.speechSynthesis;
         if (synth.speaking) {
@@ -116,19 +116,19 @@ function playSystemDesign() {
         }
 
         const defaultTimeInterval = 40000;
-        wait(2000);
+        await wait(2000);
         for (let i = 0; i < cards.length; i++) {
             const utterFront = new SpeechSynthesisUtterance(cards[i]['front']);
             // utterFront.rate = rate.value;
             utterFront.rate = 0.8;
             synth.speak(utterFront);
-            wait(cards[i]['playAfter'] || defaultTimeInterval);
+            await wait(cards[i]['playAfter'] || defaultTimeInterval);
 
             const utterBack = new SpeechSynthesisUtterance(cards[i]['back']);
             // utterBack.rate = rate.value;
             utterBack.rate = 0.8;
             synth.speak(utterBack);
-            wait(cards[i]['playAfter'] || defaultTimeInterval);
+            await wait(cards[i]['playAfter'] || defaultTimeInterval);
         }
     } catch (e) {
         alert(e)
